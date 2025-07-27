7b0ab917bbd9be6d3d9ad7dc307f7a7b2dbf474b
nikilok <nikilok@gmail.com>
2025-07-27 12:53:34 +0100

    use the getRanksByColumn to sort by gold for now and limit to top 10

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,20 @@ import medalsDataJson from "./lib/medals.json";
 import { getCountryTotals } from "./lib/getCountryTotals";
 import type { MedalsData } from "./types/medals";
 import MedalsTable from "./components/MedalsTable";
+import { getRanksByColumn } from "./lib/SortingUtils";
 
 export default async function Home() {
   const medalsData = medalsDataJson as MedalsData;
   const medalsDataWithTotals = await getCountryTotals(medalsData);
 
-  const sortedData = [...medalsDataWithTotals];
-
+  const top10SortedData = getRanksByColumn(medalsDataWithTotals, 'gold').slice(0, 10)
   return (
     <div className="font-sans min-h-screen p-8">
       <h1 className="text-3xl font-bold mb-8 text-center">
         Olympics Medal Rankings
       </h1>
 
-      <MedalsTable data={sortedData} />
+      <MedalsTable data={top10SortedData} />
     </div>
   );
 }
